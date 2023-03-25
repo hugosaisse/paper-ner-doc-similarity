2b6c112512194e0bc1971bfffab28793769be353
Hugo (Mentzingen) Silva <55488468+hugosaisse@users.noreply.github.com>
2023-03-25 17:19:43 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,69 @@
+from text_preprocessor.text_preprocessor import TextPreprocessor
+import numpy as np
+import os
+import pandas as pd
+import torch
+from torch import nn
+
+# Preprocess and Tokenize
+model_path = 'dominguesm/legal-bert-ner-base-cased-ptbr'
+preprocessor = TextPreprocessor(model_path)
+df = pd.read_csv('./data/data.csv')
+preprocessed_df = preprocessor.preprocess_text(df, raw_text_column='docContSplitted')
+
+
+
+
+
+# List infractions evaluated by experts (for holdout sample)
+df_expert1 = pd.read_excel('./data/similarity_experts_evaluation_i.xlsm', sheet_name='Similaridade')
+holdout_violations = np.unique(df_expert1[['Inf A', 'Inf B']].values)
+
+# Select infractions for fine-tuning the Word2Vec FastText model
+df_train = preprocessed_df[~preprocessed_df['infracaoId'].isin(holdout_violations)]
+df_train.reset_index(drop=True, inplace=True)
+
+# Check that MPS (MacOS M1 Pro) is available, use GPU if available
+if not torch.backends.mps.is_available():
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+    device = torch.device("cpu")
+else:
+    device = torch.device("mps")
+
+# Convert FastText model to PyTorch
+pretrained_model_path = './models/cc.pt.300.bin'
+ft_model = fasttext.load_model(pretrained_model_path)
+pt_model = FastText(ft_model)
+pt_model.to(device)
+
+# Convert dataset to PyTorch format
+train_dataset = LegalTextDataset(df_train)
+train_loader = DataLoader(train_dataset, batch_size=64, collate_fn=collate_fn)
+
+# Fine-tune the PyTorch model on the legal text dataset
+optimizer = torch.optim.Adam(pt_model.parameters(), lr=1e-4)
+criterion = nn.CrossEntropyLoss()
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    pt_model.train()
+    for batch_idx, (texts, labels) in enumerate(train_loader):
+        texts = texts.to(device)
+        labels = labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = pt_model(texts)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        if batch_idx % 100 == 0:
+            print(f'Epoch: {epoch+1}/{num_epochs}, Batch: {batch_idx}/{len(train_loader)}, Loss: {loss.item()}')
+
+# Save the fine-tuned model
+pt_model.save_state_dict('./models/legal_text_ft.pt')
---
+++ text_preprocessor/text_preprocessor.py
@@ -0,0 +1,65 @@
+import re
+import pandas as pd
+from transformers import AutoTokenizer
+
+
+class TextPreprocessor:
+    """
+    This class preprocesses and tokenize legal text in portuguese
+    regex_entities is a list of regular expression to remove entity names, phone numbers and addresses
+    regex_abbreviations is a regular expression to remove abbreviations
+    common_words is a list of common words to be removed before tokenization
+    """
+    def __init__(self, model_path, regex_entities=None, regex_abbreviations=None, common_words=None):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.regex_entities = regex_entities
+        self.regex_abbreviations = regex_abbreviations
+        self.common_words = common_words
+
+    def preprocess_text(self, df, raw_text_column='text'):
+        """
+        This function preprocesses raw text in a DataFrame by removing common abbreviations and replacing very common words,
+        and then tokenizing the resulting text.
+
+        Input: DataFrame with columns 'id' and raw_text_column
+        Output: DataFrame with columns 'id', raw_text_column, and 'tokens'
+        """
+        if self.regex_abbreviations:
+            df[raw_text_column] = df[raw_text_column].apply(lambda x: self._remove_common_abbreviations(x, self.regex_abbreviations))
+        if self.regex_entities:
+            df[raw_text_column] = df[raw_text_column].apply(lambda x: self._regex_substitute(x, self.regex_entities))
+        if self.common_words:
+            df[raw_text_column] = df[raw_text_column].apply(lambda x: self._replace_common_words(x, self.common_words))
+        df['tokens'] = df[raw_text_column].apply(lambda x: self._tokenize_text(x))
+        return df
+
+    def _remove_common_abbreviations(self, text, regex_abbreviations):
+        """
+        This function removes common abbreviations from text using regex.
+        """
+        return ' '.join([word for word in re.split(regex_abbreviations, text) if len(word) > 1])
+
+    def _replace_common_words(self, text, common_words):
+        """
+        This function replaces very common words in text.
+        """
+        for word in common_words:
+            text = re.sub(r'\b{}\b'.format(word), '', text)
+        return text
+
+    def _regex_substitute(self, text, regex_entities):
+        """
+        This function removes the matching strings from the text based on a list of regular expressions.
+        """
+        for regex in regex_entities:
+            text = re.sub(regex, '', text)
+        return text
+
+    def _tokenize_text(self, text):
+        """
+        This function tokenizes text using FastText tokenizer.
+        """
+        inputs = self.tokenizer(text, max_length=512, truncation=True, return_tensors="pt")
+        tokens = inputs.tokens()
+        
+        return inputs, tokens
