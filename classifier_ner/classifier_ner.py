0284ae301c56bbd7f99ca26bb1033fe93e5fb2eb
Hugo (Mentzingen) Silva <55488468+hugosaisse@users.noreply.github.com>
2023-03-26 21:24:29 -0300

    Included classifier_ner module

--- classifier_ner/classifier_ner.py
+++ classifier_ner/classifier_ner.py
@@ -1,15 +1,35 @@
-from transformers import AutoModelForTokenClassification
+from transformers import AutoModelForTokenClassification, pipeline
+import pandas as pd
 import torch
 
 class Classifier:
     """
     This class performs the Named Entity Recognition
     """
-    def __init__(self, model_path):
+    def __init__(self, model_path, device=torch.device("cpu")):
+        self.model_path = model_path
+        self.device = device
         self.model = AutoModelForTokenClassification.from_pretrained(model_path)
+        self.model.to(device)
     
     def predict(self, df, inputs_column='inputs'):
-        outputs = df[inputs_column].apply(lambda x:self.model(**x).logits)
-        df['predictions'] = outputs.apply(lambda x:torch.argmax(x, dim=2))
+        outputs_list = []
+        for inputs in df[inputs_column]:
+            outputs = self.model(**inputs.to(self.device))
+            logits = outputs.logits
+            probabilities = torch.softmax(logits, dim=2).tolist()[0]
+            predicted_label = torch.argmax(logits, dim=2)
+            p_list = []
+            for p in predicted_label[0].cpu().numpy():
+                p_list.append(self.model.config.id2label[p])
+            outputs_list.append({'logits': logits.tolist()[0],
+                                 'probabilities': probabilities,
+                                 'predicted_label': predicted_label.tolist()[0],
+                                 'prediction': p_list
+                                })
+        predicted_labels = [outputs['predicted_label'] for outputs in outputs_list]
+        predictions = [outputs['prediction'] for outputs in outputs_list]
+        df['labels'] = pd.Series(predicted_labels)
+        df['predictions'] = pd.Series(predictions)
 
         return df
--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 from text_preprocessor.text_preprocessor import TextPreprocessor
 from classifier_ner.classifier_ner import Classifier
 import numpy as np
-import os
 import pandas as pd
 import torch
-from torch import nn
-
-# Preprocess and Tokenize
-model_path = 'dominguesm/legal-bert-ner-base-cased-ptbr'
-preprocessor = TextPreprocessor(model_path)
-df = pd.read_csv('./data/data.csv')
-preprocessed_df = preprocessor.preprocess_text(df, raw_text_column='docContSplitted')
-
-# Get predictions (Named Entities)
-classifier = Classifier(model_path)
-classified_df = classifier.predict(preprocessed_df)
-
-
-# List infractions evaluated by experts (for holdout sample)
-df_expert1 = pd.read_excel('./data/similarity_experts_evaluation_i.xlsm', sheet_name='Similaridade')
-holdout_violations = np.unique(df_expert1[['Inf A', 'Inf B']].values)
-
-# Select infractions for fine-tuning the Word2Vec FastText model
-df_train = preprocessed_df[~preprocessed_df['infracaoId'].isin(holdout_violations)]
-df_train.reset_index(drop=True, inplace=True)
 
 # Check that MPS (MacOS M1 Pro) is available, use GPU if available
 if not torch.backends.mps.is_available():
@@ -36,3 +15,24 @@ if not torch.backends.mps.is_available():
     device = torch.device("cpu")
 else:
     device = torch.device("mps")
+
+# Preprocess and Tokenize
+model_path = 'dominguesm/legal-bert-ner-base-cased-ptbr'
+preprocessor = TextPreprocessor(model_path)
+df = pd.read_csv('./data/data.csv')
+preprocessed_df = preprocessor.preprocess_text(df, raw_text_column='docContSplitted')
+
+# Get predictions (Named Entities)
+classifier = Classifier(model_path, device)
+classified_df = classifier.predict(preprocessed_df)
+
+
+# List infractions evaluated by experts (for holdout sample)
+df_expert1 = pd.read_excel('./data/similarity_experts_evaluation_i.xlsm', sheet_name='Similaridade')
+holdout_violations = np.unique(df_expert1[['Inf A', 'Inf B']].values)
+
+# Select infractions for fine-tuning the Word2Vec FastText model
+df_train = preprocessed_df[~preprocessed_df['infracaoId'].isin(holdout_violations)]
+df_train.reset_index(drop=True, inplace=True)
+
+
