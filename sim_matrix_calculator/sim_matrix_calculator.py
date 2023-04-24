Corrected similarity matrices calculation

sim_matrix_calculator/sim_matrix_calculator.py
@@ -1,7 +1,7 @@
 import os
 import torch
 import pandas as pd
-from torch import nn
+from sklearn.metrics.pairwise import cosine_similarity
 
 class SimilarityMatrixCalculator:
     def __init__(self, vectorized_df, columns):
@@ -10,11 +10,10 @@ class SimilarityMatrixCalculator:
         self.similarity_matrices = {}
         
     def calculate_similarity_matrices(self):
-        cos = nn.CosineSimilarity(dim=1)
-
         for column in self.columns:
-            embeddings = torch.stack(self.vectorized_df[column].tolist())
-            similarity_matrix = cos(embeddings.unsqueeze(1), embeddings.unsqueeze(0))
+            embeddings = torch.stack(self.vectorized_df[column].tolist()).numpy()
+            # Calculate similarity matrix for each document with all other documents, including itself
+            similarity_matrix = cosine_similarity(embeddings, embeddings)
             self.similarity_matrices[column] = similarity_matrix
 
     def save_similarity_matrices(self, output_folder="sim_matrices"):
@@ -23,5 +22,5 @@ class SimilarityMatrixCalculator:
 
         for column, matrix in self.similarity_matrices.items():
             output_file = os.path.join(output_folder, f"{column}_similarity.csv")
-            pd.DataFrame(matrix.numpy()).to_csv(output_file, index=False)
+            pd.DataFrame(matrix).to_csv(output_file, index=False)
 